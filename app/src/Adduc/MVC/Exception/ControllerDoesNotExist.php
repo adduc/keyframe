9decb71d0b8a5055e77eaa1d24528aa473abb02c
John Long <a@88k.us>
2013-05-12 08:03:40 -0500

    Friendly error message for ControllerDoesNotExist Exception.

--- app/src/Adduc/MVC/Exception/ControllerDoesNotExist.php
+++ app/src/Adduc/MVC/Exception/ControllerDoesNotExist.php
@@ -4,7 +4,18 @@ namespace Adduc\MVC\Exception;
 class ControllerDoesNotExist extends Exception {
 
     public function friendlyMessage() {
+        $path = str_replace("\\", "/", $this->getMessage());
+        list($ns, $class) = str_split($this->getMessage(), strrpos($path, "/"));
 
+        $msg[] = $this->getMessage() . " does not exist. <br />";
+        $msg[] = "Create a file at APP/src/{$path}.php with the following:";
+        $msg[] = "<pre><code>";
+        $msg[] = "&lt;?php";
+        $msg[] = "namespace {$ns};";
+        $msg[] = "class " . substr($class, 1) . " {";
+        $msg[] = "}";
+        $msg[] = "</code></pre>";
+        return implode("\n", $msg);
     }
 
 }
